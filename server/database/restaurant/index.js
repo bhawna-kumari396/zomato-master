0b21deff18f5dc231dc2e0fb75026cc49ec4e572
Bhawna Kumari <bhawnakumari.7870@gmail.com>
2021-10-17 20:05:05 +0530

    done with schema

---
+++ server/database/restaurant/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  address: { type: String, required: ture },
+  mapLocation: { type: String, required: ture },
+  cuisine: [String],
+  restaurantTimings: String,
+  contactNumber: Number,
+  website: String,
+  popularDishes: [String],
+  averageCost: Number,
+  amenties: [String],
+  menuImages: {
+    type: mongoose.Types.ObjectId,
+    ref: "Images",
+  },
+  menu: {
+    type: mongoose.Types.ObjectId,
+    ref: "Menus",
+  },
+
+  reviews: [{type: mongoose.Types.ObjectId, ref: "Reviews"}],
+});
